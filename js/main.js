adcc1abe38973a0e7801e446f33972bb9d30ce00
Tristen <tristen.wentling@gmail.com>
2017-01-02 12:06:04 -0600

    JQueried Up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('full_loaded');
+    }, 2000);
+    
+    $('div.jumbotron').fadeIn(2500,function(){
+        $('#lcont').fadeIn(function(){
+            $('#ccont').fadeIn(function(){
+                $('#rcont').fadeIn();
+            });
+        });
+    });
+    
+});
 
